e819effc714e1daf5fc2c7771812064f987c7601
wuchenxiuwu <116479750+wuchenxiuwu@users.noreply.github.com>
2025-02-14 18:59:27 +0800

    更新 ComponentTest.tsx

--- app/ComponentTest.tsx
+++ app/ComponentTest.tsx
@@ -11,15 +11,15 @@ import React, { useState } from 'react'
 import { ScrollView, Text, View } from 'react-native'
 
 const selectorData = [
-    { label: 'Item 0', value: '1' },
-    { label: 'Item 1', value: '1' },
-    { label: 'Item 2', value: '2' },
-    { label: 'Item 3', value: '3' },
-    { label: 'Item 4', value: '4' },
-    { label: 'Item 5', value: '5' },
-    { label: 'Item 6', value: '6' },
-    { label: 'Item 7', value: '7' },
-    { label: 'Item 8', value: '8' },
+    { label: '项目 0', value: '1' },
+    { label: '项目 1', value: '1' },
+    { label: '项目 2', value: '2' },
+    { label: '项目 3', value: '3' },
+    { label: '项目 4', value: '4' },
+    { label: '项目 5', value: '5' },
+    { label: '项目 6', value: '6' },
+    { label: '项目 7', value: '7' },
+    { label: '项目 8', value: '8' },
 ]
 
 const buttonVariants = ['primary', 'secondary', 'tertiary', 'critical', 'disabled']
@@ -41,25 +41,39 @@ const ComponentTest = () => {
             <View style={{ rowGap: 8 }}>
                 {buttonVariants.map((item) => (
                     //@ts-ignore
-                    <ThemedButton variant={item} key={item} label={`Button Variant: ${item}`} />
+                    <ThemedButton
+                        variant={item}
+                        key={item}
+                        label={
+                            item === 'primary'
+                                ? '按钮样式：主要'
+                                : item === 'secondary'
+                                ? '按钮样式：次要'
+                                : item === 'tertiary'
+                                ? '按钮样式：辅助'
+                                : item === 'critical'
+                                ? '按钮样式：关键'
+                                : '按钮样式：禁用'
+                        }
+                    />
                 ))}
             </View>
-            <Accordion label="Test Accordion">
-                <Text style={{ color: 'yellow' }}>TEST TEXT</Text>
+            <Accordion label="测试手风琴">
+                <Text style={{ color: 'yellow' }}>测试文本</Text>
             </Accordion>
-            <StringArrayEditor title="Test Input Label" value={data} setValue={setData} />
-            <ThemedCheckbox label="Test Checkbox" value={checkbox} onChangeValue={setCheckbox} />
-            <ThemedSwitch label="Test Switch" value={sw} onChangeValue={setSw} />
+            <StringArrayEditor title="测试输入标签" value={data} setValue={setData} />
+            <ThemedCheckbox label="测试复选框" value={checkbox} onChangeValue={setCheckbox} />
+            <ThemedSwitch label="测试开关" value={sw} onChangeValue={setSw} />
             <ThemedSlider
                 value={slider}
                 onValueChange={setSlider}
                 min={0}
                 max={10}
                 step={1}
-                label="Test Slider"
+                label="测试滑块"
             />
             <ThemedTextInput
-                label="Test Text"
+                label="测试文本"
                 value={textInputData}
                 onChangeText={setTextInputData}
             />
