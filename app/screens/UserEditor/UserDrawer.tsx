b899d02d7c7b3ee82f1b0fb76170e3c6e6e29599
wuchenxiuwu <116479750+wuchenxiuwu@users.noreply.github.com>
2025-02-14 19:21:24 +0800

    更新 UserDrawer.tsx

--- app/screens/UserEditor/UserDrawer.tsx
+++ app/screens/UserEditor/UserDrawer.tsx
@@ -1,33 +1,41 @@
-import Drawer from '@components/views/Drawer'
-import { SetStateAction } from 'react'
+import ThemedButton from '@components/buttons/ThemedButton'
+import ThemedText from '@components/text/ThemedText'
+import ThemedView from '@components/views/ThemedView'
+import { Theme } from '@lib/theme/ThemeManager'
 import { StyleSheet } from 'react-native'
+import { useState } from 'react'
 
-import UserList from './UserList'
+const UserList = ({ setShowModal }: { setShowModal: (b: boolean) => void }) => {
+    const { color, spacing } = Theme.useTheme()
 
-type UserDrawerProps = {
-    booleans: [boolean, (b: boolean | SetStateAction<boolean>) => void]
-}
-
-const UserDrawer: React.FC<UserDrawerProps> = ({ booleans: [showModal, setShowModal] }) => {
     return (
-        <Drawer drawerStyle={styles.drawer} setShowDrawer={setShowModal} direction="right">
-            <UserList setShowModal={setShowModal} />
-        </Drawer>
+        <ThemedView style={styles.container}>
+            <ThemedText style={styles.title}>用户</ThemedText>
+            <ThemedButton
+                label="关闭"
+                onPress={() => setShowModal(false)}
+                style={styles.closeButton}
+            />
+            {/* 用户列表项显示在此处 */}
+        </ThemedView>
     )
 }
 
-export default UserDrawer
+export default UserList
 
 const styles = StyleSheet.create({
-    drawer: {
-        width: '80%',
-        left: '20%',
-        borderTopWidth: 3,
-        elevation: 20,
-        position: 'absolute',
-        height: '100%',
-        paddingHorizontal: 16,
-        paddingTop: 16,
-        paddingBottom: 24,
+    container: {
+        flex: 1,
+        padding: 16,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+        color: color.text._100,
+    },
+    closeButton: {
+        alignSelf: 'flex-end',
+        marginTop: 16,
     },
 })
